a5da07c21d605e1eb683d0eb62111d0b8d9d6ac7
jordaneldridge <Jordan@codedifferently.com>
2022-08-27 11:27:36 -0400

    adding the front end to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import HelpDeskForm from './Components/HelpDeskForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HelpDeskForm/>
+    </>
+
+    
   );
 }
 
